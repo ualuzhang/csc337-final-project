adbb69cd7e0ec9b1647debee422f9940c53e70ec
zhenyu1234 <68035901+zhenyu1234@users.noreply.github.com>
2020-08-08 15:55:33 -0700

    Add files via upload

---
+++ code.js
@@ -0,0 +1,59 @@
+/*
+  Author: Zhenyu Yuan
+  Class: CSc337
+  get from login.zip, changed url and added a localStorage object to
+  store username.
+*/
+/*
+  function login helps for login to the home page.
+  It gaves some error msg if error.
+*/
+function login() {
+  var httpRequest = new XMLHttpRequest();
+  
+  let u = document.getElementById('lusername').value;
+  let p = document.getElementById('lpassword').value;
+
+  httpRequest.onreadystatechange = () => {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+      if (httpRequest.status === 200) {
+        if (httpRequest.responseText == 'BAD') {
+        } else {
+          let url = '/home.html';
+          localStorage.setItem('user', u);
+          window.location = url;
+        }
+        document.getElementById('warningOutput').innerHTML 
+        = "Issue logging with that info";      
+      } else { alert('ERROR'); }
+    }
+  }
+
+  httpRequest.open('GET', '/login/' + u + '/' + p, true);
+  httpRequest.send();
+}
+
+
+/*
+  function areatAccount is able to help create a new account, 
+  It alert an error if a username is already used.
+*/
+function createAccount() {
+  var httpRequest = new XMLHttpRequest();
+  httpRequest.onreadystatechange = () => {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+      if (httpRequest.status === 200) {
+        alert(httpRequest.responseText);
+      } else { alert('ERROR'); }
+    }
+  }
+
+  let u = document.getElementById('username').value;
+  let p = document.getElementById('password').value;
+  let f = document.getElementById('fav').value;
+
+  httpRequest.open('GET', '/create/' + u + '/' + p + '/' + f, true);
+  httpRequest.send();
+}
+
+
